38e26db42c1a510fe8fd75dadc90c082d508e083
Kyeongho Park <khpark@ncsoft.com>
2017-05-11 18:01:36 +0900

    LC-2537 #time 1h add clock font.

--- ncstreamer_remote_reference/src/windows_message_handler.cc
+++ ncstreamer_remote_reference/src/windows_message_handler.cc
@@ -11,6 +11,30 @@
 namespace {
 HWND static_main_dialog{NULL};
 HWND static_clock_panel{NULL};
+
+HFONT static_clock_font{NULL};
+
+
+HFONT SetUpFont(HWND wnd, int font_size) {
+  HFONT font = ::CreateFont(
+      font_size,
+      0,
+      0,
+      0,
+      FW_NORMAL,
+      FALSE,
+      FALSE,
+      FALSE,
+      DEFAULT_CHARSET,
+      OUT_DEFAULT_PRECIS,
+      CLIP_DEFAULT_PRECIS,
+      DEFAULT_QUALITY,
+      DEFAULT_PITCH | FF_DONTCARE,
+      NULL);
+
+  ::SendMessage(wnd, WM_SETFONT, (WPARAM) font, NULL);
+  return font;
+}
 }  // unnamed namespace
 
 
@@ -30,12 +54,16 @@ HWND CreateMainDialog(
   static_main_dialog = dlg;
   static_clock_panel = ::GetDlgItem(dlg, IDC_STATIC_CLOCK);
 
+  static_clock_font = SetUpFont(static_clock_panel, 36);
+
   ::ShowWindow(dlg, cmd_show);
   return dlg;
 }
 
 
 void DeleteMainDialog() {
+  ::DeleteObject(static_clock_font);
+
   ::DestroyWindow(static_main_dialog);
 }
 
