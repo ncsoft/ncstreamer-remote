3fe742a7dee6b394ca3072813be996fee6b3a987
Kyeongho Park <khpark@ncsoft.com>
2017-07-20 09:27:15 +0900

    refactoring: add empty ncstreamer_remote_message_types.cc .

---
+++ ncstreamer_remote_dll/src/ncstreamer_remote_message_types.cc
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2017 NCSOFT Corporation
+ */
+
+
+#include "ncstreamer_remote_dll/src/ncstreamer_remote_message_types.h"
+
+
+namespace ncstreamer {
+}  // namespace ncstreamer
