11bbf09e61e097cdc1e58c2cf4ea274ab4320d84
Kyeongho Park <khpark@ncsoft.com>
2017-07-25 16:20:56 +0900

    cpplint: h files in ncstreamer_remote_dll/include directory.

--- ncstreamer_remote_dll/build_events/pre_build_event.bat
+++ ncstreamer_remote_dll/build_events/pre_build_event.bat
@@ -1,3 +1,4 @@
 set RootDir=%~1
 
+call "%RootDir%/build_tools/cpplint/cpplint_directory.bat" "%GOOGLE_STYLEGUIDE_PATH%/cpplint" "%RootDir%/ncstreamer_remote_dll/include"
 call "%RootDir%/build_tools/cpplint/cpplint_directory.bat" "%GOOGLE_STYLEGUIDE_PATH%/cpplint" "%RootDir%/ncstreamer_remote_dll/src"
