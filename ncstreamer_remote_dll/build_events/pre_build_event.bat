9d8e1a1a254e6a940309ec91793cf08a3d2aea7c
Kyeongho Park <khpark@ncsoft.com>
2017-07-25 13:55:43 +0900

    rename: ROOT/vs/ncstreamer_remote_dll/build_tools -> ROOT/build_tools .

--- ncstreamer_remote_dll/build_events/pre_build_event.bat
+++ ncstreamer_remote_dll/build_events/pre_build_event.bat
@@ -1,3 +1,3 @@
 set RootDir=%~1
 
-call build_tools/cpplint/cpplint_directory.bat "%GOOGLE_STYLEGUIDE_PATH%/cpplint" "%RootDir%/ncstreamer_remote_dll/src"
+call "%RootDir%/build_tools/cpplint/cpplint_directory.bat" "%GOOGLE_STYLEGUIDE_PATH%/cpplint" "%RootDir%/ncstreamer_remote_dll/src"
